462dd73c6a4c2a89b960aad5b57c1ec3e27a955e
Arūnas Norvaiša <an@86.lt>
2018-06-11 08:47:33 +0300

    got rid of some code duplication

--- js/main.js
+++ js/main.js
@@ -7,15 +7,14 @@ $(document).ready(function() {
         let weedName = e.target.value;
         // prasideda linksmybės :)
         $.ajax({
-            url: 'http://strainapi.evanbusse.com/Q1tPKQw/strains/search/name/' +
-                weedName
+            url: 'http://strainapi.evanbusse.com/Q1tPKQw/strains/search/name/' + weedName
         }).done(function(weeds) {
             // Kuriame rezultatų masyvą, kad optimizuot paiešką
             results = [];
             $.each(weeds, function(index, weed) {
                 results.push({
                     'name': weed.name,
-                    'id': weed.id,
+                    'id'  : weed.id,
                     'race': weed.race,
                     'desc': weed.desc
                 });
@@ -35,37 +34,39 @@ $(document).ready(function() {
     });
 });
 
+function printResult (weed) {
+    $('#strains').append(`
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">${weed.name}</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-5">
+                        Strain Race: &nbsp;<span class="badge badge-danger">${weed.race}</span>
+                    </div>
+                    <div class="col-md-7">
+                        <ul class="list-group">
+                            <h6><span class="badge badge-success">Description:</span></h6>
+                            <li class="list-group-item">${weed.desc}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+}
+
 function printResults(weeds) {
     $('#strains').html(``);
     if (weeds.length > 0) {
         $.each(weeds, function(index, weed) {
-            $('#strains').append(`
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">${weed.name}</h3>
-                    </div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-md-5">
-                                Strain Race: &nbsp;<span class="badge badge-danger">${weed.race}</span>
-                            </div>
-                            <div class="col-md-7">
-                                <ul class="list-group">
-                                    <h6><span class="badge badge-success">Description:</span></h6>
-                                    <li class="list-group-item">${weed.desc}</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            `);
+            printResult(weed);
         });
     } else {
-        $('#strains').append(
-            `
-        <h4>Your search returned no results, sorry.</h4>
-        `
-        );
+        $('#strains').append(`
+            <h4>Your search returned no results, sorry.</h4>
+        `);
     }
 }
 
@@ -74,26 +75,7 @@ function filterResults(weeds) {
     let filterValue = $('#raceSelection').val();
     $.each(weeds, function(index, weed) {
         if (weed.race == filterValue) {
-            $('#strains').append(`
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">${weed.name}</h3>
-                    </div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-md-5">
-                                Strain Race: &nbsp;<span class="badge badge-danger">${weed.race}</span>
-                            </div>
-                            <div class="col-md-7">
-                                <ul class="list-group">
-                                    <h6><span class="badge badge-success">Description:</span></h6>
-                                    <li class="list-group-item">${weed.desc}</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            `);
+            printResult(weed);
         }
     });
 }
